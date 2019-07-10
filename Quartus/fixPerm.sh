b2a0b4eeae5d4f8d087af50bf0ce901f3c8f6f9f
tianyouw <tian.you.w@outlook.com>
2019-07-10 19:05:43 -0400

    Update sof, fix fixPerm.sh

--- Quartus/fixPerm.sh
+++ Quartus/fixPerm.sh
@@ -2,5 +2,7 @@
 # This script fixes permissions if they are ever reset to 000.
 # Useful when using WSL for git and Quartus on Windows.
 
+me=`basename "$0"`
+
 find . -type d -exec chmod 755 {} \;
-find . -type f -exec chmod 644 {} \;
+find . -type f ! -name "$me" -exec chmod 644 {} \;
